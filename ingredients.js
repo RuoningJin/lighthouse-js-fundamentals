6090b40e5768875d6da2684527b8f11fe37b903a
RuoningJin <ronjin2013@hotmail.com>
2022-09-30 21:41:40 -0400

    ingredients

---
+++ ingredients.js
@@ -0,0 +1,13 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++
+}
+for (j = 0; j < ingredients.length; j++) {
+  console.log(ingredients[j]);
+}
+for (n = ingredients.length - 1; n >= 0; n--) {
+  console.log(ingredients[n]);
+}
